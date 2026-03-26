dbb0b8c8c7cd16a47213bc8c528c1e74971faa03
Myra <myraisadora7@gmail.com>
2026-03-26 14:18:03 +0700

    rename dockerfile

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 # Konfigurasi AWS
+#hehe
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_SESSION_TOKEN = os.getenv("AWS_SESSION_TOKEN")
